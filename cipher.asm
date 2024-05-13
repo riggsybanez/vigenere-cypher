a7cc14344a971c05cf569c5d456ebcb05222d0dd
riggsybanez <142960686+riggsybanez@users.noreply.github.com>
2024-05-14 01:13:41 +0800

    Update cipher.asm

--- cipher.asm
+++ cipher.asm
@@ -18,36 +18,30 @@ _start:
     xor ebx, ebx        
 
 encrypt_loop:
-    ; Calculate the current key character
+   
     movzx eax, byte [edi + ebx]  
     sub eax, 'A'
     mov dl, al
 
-    ; Calculate the current plaintext character
     movzx eax, byte [esi + ebx]  
     sub eax, 'A'
 
-    ; Encrypt the current character using Vigenère cipher (arithmetic method)
     add al, dl
     and al, 0x1F
     add al, 'A'
 
-    ; Store the encrypted character in ciphertext buffer
     mov [ciphertext + ebx], al
 
-    ; Move to the next character of the keyword (cycling if necessary)
     inc ebx
     cmp ebx, keyword_len
     jl continue_loop
     mov ebx, 0  ; Reset keyword index
 
 continue_loop:
-    ; Check if there are more characters to encrypt
     cmp ebx, msglen
     jl encrypt_loop
 
 print_ciphertext:
-    ; Print the ciphertext
     mov eax, 4
     mov ebx, stdout
     mov ecx, ciphertext
